f32d9209d5c3281ac625769f11742bfc7ae947a7
Simon <chechin.simon@gmail.com>
2019-04-18 21:21:02 +0300

    end?

--- Omer.py
+++ Omer.py
@@ -1,34 +1,12 @@
-import psycopg2
 from datetime import datetime, timedelta
-import pytz, requests
+import pytz
+import requests
 
 
 URL = 'http://db.ou.org/zmanim/getCalendarData.php'
 
-def get_location_by_id(user_id: int) -> tuple:
-    with psycopg2.connect(settings.db_parameters_string) as conn:
-        cur = conn.cursor()
-        query = f'SELECT latitude, longitude FROM locations ' \
-                f'WHERE id = {user_id}'
-        cur.execute(query)
-        location = cur.fetchone()
-        response = location
-        return response
 
-
-def get_tz_by_id(user_id: int) -> str:
-    with psycopg2.connect(settings.db_parameters_string) as conn:
-        cur = conn.cursor()
-        query = f'SELECT public.tz.tz FROM public.tz WHERE id = {user_id}'
-        cur.execute(query)
-        tz = cur.fetchone()
-        response = tz[0]
-        return response
-
-
-def get_user_sunset(user_id: int) -> datetime.time:
-    tz = get_tz_by_id(user_id)
-    loc = get_location_by_id(user_id)
+def get_user_sunset(tz, loc) -> datetime.time:
     tz_time = pytz.timezone(tz)
     now = datetime.now(tz_time)
     date_str = f'{now.month}/{now.day}/{now.year}'
@@ -46,9 +24,6 @@ def get_user_sunset(user_id: int) -> datetime.time:
     delta = timedelta(hours=1)
     timer = datetime.time(timer - delta)
     return timer
-
-
-
 
 
 print(get_user_sunset("Europe/Moscow", (35.33,50.333)))
